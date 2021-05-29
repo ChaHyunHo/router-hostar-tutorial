fb6502c91ae22973835919af40ec785c9a5c2bea
chahyeonho <chamym@naver.com>
2021-05-29 21:35:58 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Link, Route} from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  return(
+      <div>
+        <ul>
+            <li>
+                <Link to="/">홈</Link>
+                {/*<a href="/">홈 에이태그</a> a태그를 사용하게되면 페이지 전환하는 과정에서 페이지를 새로불러옴 사용x(렌더링된 컴포넌트들이 모두 사라짐) */}
+            </li>
+            <li>
+                <Link to="/about?detail=true&info=cha&location=soeul">소개</Link>
+            </li>
+            <li>
+                <Link to="/info">소개2</Link>
+            </li>
+            <li>
+                <Link to="/profile/hostar">hostar 프로필</Link>
+            </li>
+            <li>
+                <Link to="/profile/mom">mom 프로필</Link>
+            </li>
+        </ul>
+        <hr />
+        <Route path="/" component={Home} exact={true} />
+        <Route path={['/about', '/info']} component={About}/>
+          <Route path="/profile/:username" component={Profile}/>
+      </div>
+  )
 }
 
 export default App;
---
+++ src/Profile.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const data = {
+    hostar : {
+        name : '차현호',
+        description : '차현호는 리액트를 공부중이다',
+    },
+
+    mom : {
+        name : '서화석',
+        description : '현호의 어무니 이름',
+    }
+
+}
+
+const Profile = ({match}) => {
+    const {username} = match.params;
+    const profile = data[username];
+
+    if(!profile) {
+        return <div>존재하지 않은 사용자입니다.</div>;
+    }
+
+    return (
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+};
+
+export default Profile;
