a06b8d15a72f6c5017227843b7908b374ede43fd
chahyeonho <chamym@naver.com>
2021-05-29 22:35:46 +0900

    init commit

--- src/About.js
+++ src/About.js
@@ -1,22 +1,24 @@
 import React from "react";
-import qs from 'qs';
+import qs from "qs";
 
-const About = ({location}) => {
-    const query = qs.parse(location.search, {
-        ignoreQueryPrefix: true // 이 설정을 통해 문자열 맨 앞의 ?를 생략
-    })
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 이 설정을 통해 문자열 맨 앞의 ?를 생략
+  });
 
-    console.log(query);
+  console.log(query);
 
-    const showDetail = query.detail === 'true'; // 쿼리의 파싱 결과 값은 문자열이다.
+  const showDetail = query.detail === "true"; // 쿼리의 파싱 결과 값은 문자열이다.
 
-    return (
-        <div>
-            <h1>소개</h1>
-            <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
-            {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
-        </div>
-    );
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
+      <p>info : {query.info === undefined ? "없음" : query.info}</p>
+      <p>location : {query.location === undefined ? "없음" : query.location}</p>
+    </div>
+  );
 };
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import React from "react";
-import {Link, Route} from 'react-router-dom';
+import { Link, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profile from "./Profile";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 const App = () => {
-
-  return(
-      <div>
-        <ul>
-            <li>
-                <Link to="/">홈</Link>
-                {/*<a href="/">홈 에이태그</a> a태그를 사용하게되면 페이지 전환하는 과정에서 페이지를 새로불러옴 사용x(렌더링된 컴포넌트들이 모두 사라짐) */}
-            </li>
-            <li>
-                <Link to="/about?detail=true&info=cha&location=soeul">소개</Link>
-            </li>
-            <li>
-                <Link to="/info">소개2</Link>
-            </li>
-            <li>
-                <Link to="/profile/hostar">hostar 프로필</Link>
-            </li>
-            <li>
-                <Link to="/profile/mom">mom 프로필</Link>
-            </li>
-        </ul>
-        <hr />
-        <Route path="/" component={Home} exact={true} />
-        <Route path={['/about', '/info']} component={About}/>
-          <Route path="/profile/:username" component={Profile}/>
-      </div>
-  )
-}
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+          {/*<a href="/">홈 에이태그</a> a태그를 사용하게되면 페이지 전환하는 과정에서 페이지를 새로불러옴 사용x(렌더링된 컴포넌트들이 모두 사라짐) */}
+        </li>
+        <li>
+          <Link to="/about?detail=true&info=cha&location=soeul">소개</Link>
+        </li>
+        <li>
+          <Link to="/info">소개2</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
+    </div>
+  );
+};
 
 export default App;
